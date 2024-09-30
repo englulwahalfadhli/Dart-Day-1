70c63e23e0011e2ab01725fe542cf0f77c080838
Lulwah Alfadhi <lulubalfadhli@gmail.com>
2024-09-30 12:28:32 +0300

    variables

--- main.dart
+++ main.dart
@@ -1,9 +1,4 @@
 void main() {
-  double fahrenheitToCelsius(double fahrenheit) {
-    return (fahrenheit - 32) / 1.8;
-  }
-
-  double fahrenheit = 86;
-  double celsius = fahrenheitToCelsius(fahrenheit);
-  print('$fahrenheit°F is equal to ${celsius.toStringAsFixed(0)}°C');
+  String myCountry = "Kuwait";
+  print(myCountry.toLowerCase().contains("k"));
 }
